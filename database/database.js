f2e945c8dda2ea7759c1b5f1d4df31992d72065b
Shubham Singh <127003248@sastra.ac.in>
2025-07-04 09:46:12 +0530

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const urr=process.env.MONGO_URI
+const urr="mongodb+srv://pawankumary122005:r8RJndoHgnYkwGWM@e-commerce-cluster.lycbojj.mongodb.net/E-CommerceDB?retryWrites=true&w=majority&appName=e-commerce-cluster"
 //this is just for connecting to the database common to all collections
 const connectDB=async ()=>
 {
